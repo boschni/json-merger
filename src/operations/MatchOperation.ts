400cccad031e7e0491d2a3094d816fec11194ae8
Dmytro Sheinin <dsprogr@gmail.com>
2020-12-09 09:01:30 +0100

    fixes fromatings

--- src/operations/MatchOperation.ts
+++ src/operations/MatchOperation.ts
@@ -3,75 +3,74 @@ import { JsonPointer } from "json-ptr";
 import Operation from "./Operation";
 
 export default class MatchOperation extends Operation {
-    name() {
-        return "match";
+  name() {
+    return "match";
+  }
+
+  processInArray(
+    keyword: string,
+    source: any,
+    sourceArray: any[],
+    sourceArrayIndex: number,
+    resultArray: any[],
+    resultArrayIndex: number,
+    target: any[]
+  ) {
+    const keywordValue: MatchKeywordValue = source[keyword];
+
+    let matchedResultArrayIndex: number;
+
+    // Handle $match.index
+    if (keywordValue.index !== undefined) {
+      matchedResultArrayIndex =
+        keywordValue.index === "-"
+          ? resultArray.length - 1
+          : keywordValue.index;
     }
 
-    processInArray(
-        keyword: string,
-        source: any,
-        sourceArray: any[],
-        sourceArrayIndex: number,
-        resultArray: any[],
-        resultArrayIndex: number,
-        target: any[]
-    ) {
-        const keywordValue: MatchKeywordValue = source[keyword];
+    // Handle $match.query
+    else if (keywordValue.query !== undefined) {
+      // Try to find a matching item in the result
+      const path = jsonpath.paths(resultArray, keywordValue.query)[0];
+      matchedResultArrayIndex =
+        path !== undefined ? (path[1] as number) : undefined;
+    }
 
-        let matchedResultArrayIndex: number;
-
-        // Handle $match.index
-        if (keywordValue.index !== undefined) {
-            matchedResultArrayIndex =
-                keywordValue.index === "-"
-                    ? resultArray.length - 1
-                    : keywordValue.index;
-        }
-
-        // Handle $match.query
-        else if (keywordValue.query !== undefined) {
-            // Try to find a matching item in the result
-            const path = jsonpath.paths(resultArray, keywordValue.query)[0];
-            matchedResultArrayIndex =
-                path !== undefined ? (path[1] as number) : undefined;
-        }
-
-        // Handle $match.path
-        else if (keywordValue.path !== undefined) {
-            // Try to find a matching item in the result
-            if (JsonPointer.get(resultArray, keywordValue.path) !== undefined) {
-                const decodedPath = JsonPointer.decode(keywordValue.path)[0];
-
-                matchedResultArrayIndex = parseInt(decodedPath.toString());
-            }
-        }
-
-        // Ignore the item if no match found
-        if (
-            matchedResultArrayIndex === undefined ||
-            resultArray[matchedResultArrayIndex] === undefined
-        ) {
-            return { resultArray, resultArrayIndex };
-        }
-
-        // Process result array item
-        const result = this._processor.processArrayItem(
-            keywordValue.value,
-            sourceArray,
-            sourceArrayIndex,
-            resultArray,
-            matchedResultArrayIndex,
-            target
+    // Handle $match.path
+    else if (keywordValue.path !== undefined) {
+      // Try to find a matching item in the result
+      if (JsonPointer.get(resultArray, keywordValue.path) !== undefined) {
+        matchedResultArrayIndex = Number(
+          JsonPointer.decode(keywordValue.path)[0]
         );
-
-        // Check if an array item has been inserted or removed below or at the current array item
-        if (matchedResultArrayIndex <= resultArrayIndex) {
-            resultArrayIndex +=
-                result.resultArrayIndex - matchedResultArrayIndex;
-        }
-
-        return { resultArray: result.resultArray, resultArrayIndex };
+      }
     }
+
+    // Ignore the item if no match found
+    if (
+      matchedResultArrayIndex === undefined ||
+      resultArray[matchedResultArrayIndex] === undefined
+    ) {
+      return { resultArray, resultArrayIndex };
+    }
+
+    // Process result array item
+    const result = this._processor.processArrayItem(
+      keywordValue.value,
+      sourceArray,
+      sourceArrayIndex,
+      resultArray,
+      matchedResultArrayIndex,
+      target
+    );
+
+    // Check if an array item has been inserted or removed below or at the current array item
+    if (matchedResultArrayIndex <= resultArrayIndex) {
+      resultArrayIndex += result.resultArrayIndex - matchedResultArrayIndex;
+    }
+
+    return { resultArray: result.resultArray, resultArrayIndex };
+  }
 }
 
 /*
@@ -79,8 +78,8 @@ export default class MatchOperation extends Operation {
  */
 
 export interface MatchKeywordValue {
-    index?: number | "-"; // the index to match against, use '-' to match on the last item
-    path?: string; // the json pointer to match against
-    query?: string; // the json path to match against
-    value: any; // the operation or value to use if a match is found
+  index?: number | "-"; // the index to match against, use '-' to match on the last item
+  path?: string; // the json pointer to match against
+  query?: string; // the json path to match against
+  value: any; // the operation or value to use if a match is found
 }
